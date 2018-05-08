11314c9dce844eb9c45209b68566124f562c72fc
yachang <changya@gmail.com>
2018-05-08 13:28:20 -0400

    merge 2 cron jobs into 1

--- server/mlabns/handlers/update.py
+++ server/mlabns/handlers/update.py
@@ -118,6 +118,8 @@ class SiteRegistrationHandler(webapp.RequestHandler):
                     logging.error('Error updating site %s.',
                                   site[self.SITE_FIELD])
                     continue
+        # call check_ip job at the end of check_site job
+        IPUpdateHandler().get()
 
         return util.send_success(self)
 
@@ -166,6 +168,12 @@ class SiteRegistrationHandler(webapp.RequestHandler):
 
                 sliver_tool = IPUpdateHandler().initialize_sliver_tool(
                     tool, site, server_id, fqdn)
+                if not memcache.set(
+                        tool.tool_id,
+                        sliver_tool,
+                        namespace=constants.MEMCACHE_NAMESPACE_TOOLS):
+                    logging.error(
+                        'Failed to update sliver IP addresses in memcache.')
                 try:
                     sliver_tool.put()
                     logging.info('Succeeded to write sliver %s to datastore.',
